1fb43a079627419e9146da61c57d6fd2bb0b2d15
kaziabulhasib <hasibkazi420@gmail.com>
2025-01-09 11:08:40 +0530

    theme config

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View className='flex-1 items-center justify-center bg-red-100 '>
+    <View className='flex flex-1 items-center justify-center bg-red-100 '>
       <Text className=''>Aura</Text>
       <StatusBar style='auto' />
       <Link className='text-center text-blue-900' href='/profile'>
